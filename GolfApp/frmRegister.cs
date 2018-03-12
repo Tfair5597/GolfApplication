c3cc6974ef3d1717e5bd2653c4c1e91b3e8276dc
Jasper-Lawliet <Tfair5597@users.noreply.github.com>
2018-03-12 14:44:01 -0700

    Added some validation to the create user form. Passwords must match and be at least 6 characters long.

--- GolfApp/frmRegister.cs
+++ GolfApp/frmRegister.cs
@@ -19,39 +19,59 @@ namespace GolfApp
 
         private void btnSaveUser_Click(object sender, EventArgs e)
         {
+            //create new golfDB to connect to DB?
             GolfDB db = new GolfDB();
 
             //create user to be saved
             User user = new User();
 
 
-            //check to see if user is already present by username
-            User testUser = DBHelper.GetUserByUsername(txtUsername.Text);
-            if (testUser == null)
+            if (txtPassword.Text != txtPassConfirmation.Text)
             {
-                User saveUser = new User(); 
-                saveUser.Username = txtUsername.Text;
-                saveUser.Password = txtPassword.Text;
+                txtPassNotMatched.Visible = true;
+            }
 
-                db.Users.Add(saveUser);
-                if (db.SaveChanges() == 1)
+            else if (txtPassword.Text == txtPassConfirmation.Text && txtPassword.Text.Length >= 6)
+            {
+                //check to see if user is already present by username
+                User testUser = DBHelper.GetUserByUsername(txtUsername.Text);
+                if (testUser == null)
                 {
-                    MessageBox.Show("User saved!");
+                    User saveUser = new User();
+                    saveUser.Username = txtUsername.Text;
+                    saveUser.Password = txtPassword.Text;
+
+                    db.Users.Add(saveUser);
+                    if (db.SaveChanges() == 1)
+                    {
+                        MessageBox.Show("User saved!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("User already exists!");
                 }
             }
             else
             {
-                MessageBox.Show("User already exists!");
+                lblPassCharacter.Visible = true;
             }
-            
-    
-
-
         }
 
         private void btnBackToLogin_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            lblPassCharacter.Visible = false;
+            txtPassNotMatched.Visible = false;
+        }
+
+        private void txtPassConfirmation_TextChanged(object sender, EventArgs e)
+        {
+            txtPassNotMatched.Visible = false;
         }
     }
 }
